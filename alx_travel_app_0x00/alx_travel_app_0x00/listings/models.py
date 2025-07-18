67a2a41ca51877955bf6c66e1b30a2da82ef8e56
Emmanuel5012 <osomisakin@gmail.com>
2025-07-18 18:16:11 +0100

    renaming directories and creating listings

---
+++ alx_travel_app_0x00/alx_travel_app_0x00/listings/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+# Create your models here.
+class TestModel(models.Model):
+    name = models.CharField(max_length=100)
+
+class Listing(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class Booking(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='reviews')
+    reviewer_name = models.CharField(max_length=100)
+    reviewer_email = models.EmailField(max_length=100)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review for {self.reviewer_name} review for {self.listing.title}"
