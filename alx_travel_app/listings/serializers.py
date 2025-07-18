7ba09e4cc370631e3fbc51bf0b4be240e1b71063
Emmanuel5012 <osomisakin@gmail.com>
2025-07-18 18:50:00 +0100

    Removing Review serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -14,9 +14,3 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = '__all__'
