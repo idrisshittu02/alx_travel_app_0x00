49533629c732feff04b504285bd80e2912eb280c
idrisshittu02 <115847693+idrisshittu02@users.noreply.github.com>
2025-07-20 23:47:17 +0100

    Update serializers.py3

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-from .models import Listing
 
-class ListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Listing
-        fields = '__all__'
+class DummySerializer(serializers.Serializer):
+    title = serializers.CharField()
