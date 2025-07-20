8a27f97b4017c2732c9351fb96c65ab3e227996d
idrisshittu02 <115847693+idrisshittu02@users.noreply.github.com>
2025-07-20 23:43:36 +0100

    Update models.py2

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-# Create your models here.
-class TestModel(models.Model):
-    name = models.CharField(max_length=100)
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
 
 class Listing(models.Model):
     title = models.CharField(max_length=200)
